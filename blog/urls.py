dc63d1da80f2fbe343449d160d989be594e7b7fd
Carlos <Lope922@gmail.com>
2016-03-15 15:44:45 -0500

    adjusted file paths for commit

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,6 @@
 from django.conf.urls import url 
-from . import views
+from . import views 
 
-
-#post_list identifies the name of the url that will be usedd to identify the view
 urlpatterns = [
-	url(r'^$', views.post_list, name='post_list'),
+	url(r'^$', views.post_list, name ='post_list'),
 ]
