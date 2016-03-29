f9312a68d13399b843285e4a0841602af49b7fa2
CaLs_Rig <Lope922@gmail.com>
2016-03-29 15:18:40 -0500

    Added view and template for detailed blog post as well as CSS for the site. Fixed url path in mysite/urls

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url 
 from . import views 
 
+#this defines the pattern that the url will have based on primary keys assigned through the database.
 urlpatterns = [
-    url(r'^$', views.post_list, name ='post_list'),
+    url(r'^$', views.post_list, name='post_list'),
     url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
--- mysite/urls.py
+++ mysite/urls.py
@@ -16,10 +16,15 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf.urls import url
+from blog import views
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-	# causes urls to redirect all requests for default page to blog
-	url(r'', include('blog.urls')),
+    # causes urls to redirect all requests for default page to blog
+    url(r'', include('blog.urls')),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 
 ]
